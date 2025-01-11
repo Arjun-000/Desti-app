b26b94aeccffed5054baf2df3a375f8971f21f6f
Arjun R <85290876+Arjun-000@users.noreply.github.com>
2025-01-11 12:50:09 +0530

    Update Destination.jsx

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DestinationCard from '../components/DestinationCard'
 import AddDestination from '../components/AddDestination'
-
+import Header from '../components/Header'
 
 const Destination = () => {
   return (
+    <>
+         <Header outsideHome={true}/>
     <div style={{paddingTop:'100px'}} className='container-fluid'>
         <div className='d-flex justify-content-between pe-5'>
           <h1>Destinations</h1>
@@ -13,6 +15,7 @@ const Destination = () => {
         </div>
         <DestinationCard/>
     </div>
+    </>
   )
 }
 
